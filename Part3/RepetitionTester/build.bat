fa3c60d8ec34221a80e568bb511e93e121159383
Juanes Rayo Work <lufi2544@gmail.com>
2025-04-03 10:56:42 +0200

    Added envinronment set up for windows

--- Part3/RepetitionTester/build.bat
+++ Part3/RepetitionTester/build.bat
@@ -1,12 +1,17 @@
-set SRC_DIR=C:\Users\juanes.rayo\Desktop\Programming\ComputerEnhace\Part3\RepetitionTester
-set BIN_DIR=%SRC_DIR%\bin
+@echo off
+
+set SRC_DIR=%~dp0/source
+set BIN_DIR=%~dp0/bin
 set CFLAGS=/std:c++17 /Zi
 set SRC_FILES=part3.cpp
 set CE_DIR=../../../../ComputerEnhace
+set CE_DIR_1=%CE_DIR%/Part1
+set CE_DIR_2=%CE_DIR%/Part2
+
 
 if not exist %BIN_DIR% mkdir %BIN_DIR%
 cd %BIN_DIR%
 
-call "../../../setup_cl_x64.bat"
+call "setup_cl_x64.bat"
 
-cl /Fe:RepetitionTester.exe %CFLAGS% -I%SRC_DIR% -I%CE_DIR% %SRC_DIR%\%SRC_FILES%
+cl /Fe:RepetitionTester.exe %CFLAGS% -I%SRC_DIR% -I%CE_DIR_1% -I%CE_DIR_2% -I%CE_DIR% %SRC_DIR%\%SRC_FILES% /nologo
